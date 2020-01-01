fb814e81cc38cd3c90f60a2f7545a88dddf54839
jonariv09 <ingjoseagustinnarvaez@gmail.com>
2020-01-01 16:29:19 -0600

    TINI

---
+++ ClientApp/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import NavBar from './components/Structure/NavBar'
+
+export default class App extends Component {
+    static displayName = App.name;
+
+    render() {
+        return (
+            <div>
+                
+
+                <NavBar />
+                
+            </div>
+        );
+    }
+}
